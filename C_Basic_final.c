f8ebf86e83374699ec6f6519a9af16681a4b96e0
NguyenMinhTan <101338037+TayHaiSaoTroi@users.noreply.github.com>
2023-11-28 12:45:47 +0700

    Update C_Basic_final.c

--- C_Basic_final.c
+++ C_Basic_final.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
-#include <stdint.h>
 
-float ham_Mu (int x,int y);
-float ham_can(int x, int y);
-void tachChuoi(char array[]);
-void giaiPhuongTrinhBacHai();
-int main(int argc, char const *argv[])
-{
-    int power=ham_Mu(2,3);
-    printf("%d",power);    
+float canBacHai(float n) {
+    if (n == 0 || n == 1)
+        return n;
+
+    float x = n;
+    float y = 1;
+    while (x - y > 0) {
+        x = (x + y) / 2;
+        y = n / x;
+    }
+    return x;
+}
+
+int main() {
+    float a, b, c;
+    float delta, x1, x2;
+    printf("Nhap a, b, c: ");
+    scanf("%f %f %f", &a, &b, &c);
+    delta = b * b - 4 * a * c;
+    if (delta > 0) {
+        x1 = (-b + canBacHai(delta)) / (2 * a);
+        x2 = (-b - canBacHai(delta)) / (2 * a);
+        printf("Phuong trinh co hai nghiem phan biet: x1 = %.2f, x2 = %.2f\n", x1, x2);
+    } else if (delta == 0) {
+        x1 = x2 = -b / (2 * a);
+        printf("Phuong trinh co nghiem kep: x1 = x2 = %.2f\n", x1);
+    } else {
+        printf("Phuong trinh vo nghiem.\n");
+    }
+
     return 0;
 }
-float ham_Mu(int x, int y)
-{
-    int ketQua;
-    for(int i=0;i<=y;++i)
-    {
-        ketQua *=x; 
-    }
-    return ketQua;
-}
-
